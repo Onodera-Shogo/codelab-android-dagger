0f0face14ed4baaf4eba63a0497e2d54563a87c2
Shogo Onodera <onodera@nttr-tech.co.jp>
2025-03-21 09:33:16 +0900

    feat: dagger実装

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,34 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.storage.Storage
+import com.example.android.dagger.user.UserManager
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
+interface AppComponent {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+    fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
+
+    fun userManager(): UserManager
+
+//    fun inject(activity: SettingsActivity)
+    //    fun inject(activity: RegistrationActivity)
+//    fun inject(fragment: EnterDetailsFragment)
+//    fun inject(fragment: TermsAndConditionsFragment)
+//    fun inject(activity: MainActivity)
+}
--- app/src/main/java/com/example/android/dagger/main/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dagger.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.UserManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -26,10 +27,15 @@ import com.example.android.dagger.R
 import com.example.android.dagger.login.LoginActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.settings.SettingsActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainViewModel: MainViewModel
+//    @Inject
+//    lateinit var userManager: UserManager
+
+    @Inject
+    lateinit var mainViewModel: MainViewModel
 
     /**
      * If the User is not registered, RegistrationActivity will be launched,
@@ -39,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val userManager = (application as MyApplication).userManager
+        val userManager = (application as MyApplication).appComponent.userManager()
         if (!userManager.isUserLoggedIn()) {
             if (!userManager.isUserRegistered()) {
                 startActivity(Intent(this, RegistrationActivity::class.java))
@@ -51,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             setContentView(R.layout.activity_main)
 
-            mainViewModel = MainViewModel(userManager.userDataRepository!!)
+//            mainViewModel = MainViewModel(userManager.userDataRepository!!)
             setupViews()
         }
     }
